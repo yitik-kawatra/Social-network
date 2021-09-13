08339112c920998e922442efb6b445f27dbddba5
Yitik Kawatra <yitikkawatra@gmail.com>
2021-09-14 02:22:19 +0530

    fix

--- src/post/SinglePost.js
+++ src/post/SinglePost.js
@@ -1,32 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { singlePost,remove } from "./apiPost";
+import { singlePost, remove } from "./apiPost";
 import DefaultPost from "../images/tree.webp";
-import { Link,Redirect } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { isAuthenticated } from "../core/Menu";
 
 function SinglePost(props) {
   const [post, setPost] = useState("");
-  const [redirects,setRedirects]=useState({redirectToHome: false,redirectToSignin: false})
-
+  const [redirects, setRedirects] = useState({
+    redirectToHome: false,
+    redirectToSignin: false,
+  });
 
   const deletePost = () => {
-    const postId = post._id
+    const postId = post._id;
     const token = isAuthenticated().token;
-    remove(postId, token).then(data => {
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            setRedirects({...redirects,redirectToHome:true})
-        }
+    remove(postId, token).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setRedirects({ ...redirects, redirectToHome: true });
+      }
     });
-};
+  };
 
-const deleteConfirmed = () => {
-  let answer = window.confirm('Are you sure you want to delete your post?');
-  if (answer) {
-    deletePost();
-  }
-};
+  const deleteConfirmed = () => {
+    let answer = window.confirm("Are you sure you want to delete your post?");
+    if (answer) {
+      deletePost();
+    }
+  };
 
   const renderPost = (post) => {
     const posterId = post.postedBy ? `/user/${post.postedBy._id}` : "";
@@ -55,16 +57,23 @@ const deleteConfirmed = () => {
         <Link to="/" className="btn btn-raised btn-primary btn-sm me-5">
           Back to posts
         </Link>
-        {isAuthenticated().user && isAuthenticated().user._id === post.postedBy._id && (
-                        <>
-                              <Link to={`/post/${post._id}`} className="btn btn-raised btn-warning btn-sm me-5">
-                                Update Post
-                            </Link>
-                            <button onClick={deleteConfirmed} className="btn btn-raised btn-danger btn-sm">
-                                Delete Post
-                            </button>
-                        </>
-                    )}
+        {isAuthenticated().user &&
+          isAuthenticated().user._id === post.postedBy._id && (
+            <>
+              <Link
+                to={`/post/edit/${post._id}`}
+                className="btn btn-raised btn-warning btn-sm me-5"
+              >
+                Update Post
+              </Link>
+              <button
+                onClick={deleteConfirmed}
+                className="btn btn-raised btn-danger btn-sm"
+              >
+                Delete Post
+              </button>
+            </>
+          )}
       </div>
     );
   };
@@ -78,15 +87,13 @@ const deleteConfirmed = () => {
       }
     });
   }, []);
- 
 
   if (redirects.redirectToHome) {
-    return <Redirect to={`/`} />; 
+    return <Redirect to={`/`} />;
   } else if (redirects.redirectToSignin) {
     return <Redirect to={`/signin`} />;
   }
   return (
-
     <div className="container">
       <h2 className="display-2 mt-5 mb-5">{post.title}</h2>
 
