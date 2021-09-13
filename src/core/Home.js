27681694154e28d49e6465169f0e81c410d1ed5a
Yitik Kawatra <yitikkawatra@gmail.com>
2021-09-13 19:27:45 +0530

    title change

--- src/core/Home.js
+++ src/core/Home.js
@@ -6,7 +6,6 @@ function Home() {
         <div>
             <div className="jumbotron">
             <h2>Home</h2>
-            <p className="lead">Welcome to React Frontend </p>
             </div>
             <div className="container">
                 <Posts />
